14ce5af567e682d498f21aaad1b140dd903d1137
Rich Archer <richard.archer@prodigyfinance.com>
2017-04-25 09:58:05 +0200

    Revert "Remove typography"
    This reverts commit c1d5cae9926f0bf570af9f65b7d5cead0fdc0855.

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,94 +4,10 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/css_kata_Boilerplate_Code_sections_0_boilerplate-sections_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Boilerplate_Code_sections_0_boilerplate-sections_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Boilerplate_Code_sections_0_boilerplate-sections_0_phone.png",
         "selector": "#boilerplate-sections",
         "fileName": "css_kata_Boilerplate_Code_sections_0_boilerplate-sections_0_phone.png",
         "label": "Boilerplate: Code sections",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 11,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/css_kata_Display_Stack_0_display-stack-images_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Display_Stack_0_display-stack-images_0_phone.png",
-        "selector": "#display-stack-images",
-        "fileName": "css_kata_Display_Stack_0_display-stack-images_0_phone.png",
-        "label": "Display: Stack",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 27,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/css_kata_Display_Align_0_display-align-images_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Display_Align_0_display-align-images_0_phone.png",
-        "selector": "#display-align-images",
-        "fileName": "css_kata_Display_Align_0_display-align-images_0_phone.png",
-        "label": "Display: Align",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 9,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/css_kata_Display_Align_and_resize_0_display-align-resize-images_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Display_Align_and_resize_0_display-align-resize-images_0_phone.png",
-        "selector": "#display-align-resize-images",
-        "fileName": "css_kata_Display_Align_and_resize_0_display-align-resize-images_0_phone.png",
-        "label": "Display: Align and resize",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 8,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/css_kata_Box_model__0_box-model-simple-box_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Box_model__0_box-model-simple-box_0_phone.png",
-        "selector": "#box-model-simple-box",
-        "fileName": "css_kata_Box_model__0_box-model-simple-box_0_phone.png",
-        "label": "Box model: ",
         "misMatchThreshold": 0.1,
         "diff": {
           "isSameDimensions": true,
@@ -108,32 +24,11 @@ report({
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/css_kata_Box_model__0_box-model-simple-border-box_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Box_model__0_box-model-simple-border-box_0_phone.png",
-        "selector": "#box-model-simple-border-box",
-        "fileName": "css_kata_Box_model__0_box-model-simple-border-box_0_phone.png",
-        "label": "Box model: ",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 16,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/css_kata_Box_model__0_box-model-simple-border-margin-box_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Box_model__0_box-model-simple-border-margin-box_0_phone.png",
-        "selector": "#box-model-simple-border-margin-box",
-        "fileName": "css_kata_Box_model__0_box-model-simple-border-margin-box_0_phone.png",
-        "label": "Box model: ",
+        "reference": "../bitmaps_reference/css_kata_Typography_101_Body_text_0_typography-body-font_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Typography_101_Body_text_0_typography-body-font_0_phone.png",
+        "selector": "#typography-body-font",
+        "fileName": "css_kata_Typography_101_Body_text_0_typography-body-font_0_phone.png",
+        "label": "Typography 101: Body text",
         "misMatchThreshold": 0.1,
         "diff": {
           "isSameDimensions": true,
@@ -150,8 +45,176 @@ report({
     },
     {
       "pair": {
+        "reference": "../bitmaps_reference/css_kata_Typography_101_Headings_0_typography-headings_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Typography_101_Headings_0_typography-headings_0_phone.png",
+        "selector": "#typography-headings",
+        "fileName": "css_kata_Typography_101_Headings_0_typography-headings_0_phone.png",
+        "label": "Typography 101: Headings",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 32,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/css_kata_Typography_101_Faux_Headings_0_typography-faux-headings_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Typography_101_Faux_Headings_0_typography-faux-headings_0_phone.png",
+        "selector": "#typography-faux-headings",
+        "fileName": "css_kata_Typography_101_Faux_Headings_0_typography-faux-headings_0_phone.png",
+        "label": "Typography 101: Faux Headings",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 17,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/css_kata_Display_Stack_0_display-stack-images_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Display_Stack_0_display-stack-images_0_phone.png",
+        "selector": "#display-stack-images",
+        "fileName": "css_kata_Display_Stack_0_display-stack-images_0_phone.png",
+        "label": "Display: Stack",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 21,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/css_kata_Display_Align_0_display-align-images_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Display_Align_0_display-align-images_0_phone.png",
+        "selector": "#display-align-images",
+        "fileName": "css_kata_Display_Align_0_display-align-images_0_phone.png",
+        "label": "Display: Align",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 12,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/css_kata_Display_Align_and_resize_0_display-align-resize-images_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Display_Align_and_resize_0_display-align-resize-images_0_phone.png",
+        "selector": "#display-align-resize-images",
+        "fileName": "css_kata_Display_Align_and_resize_0_display-align-resize-images_0_phone.png",
+        "label": "Display: Align and resize",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 9,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/css_kata_Box_model__0_box-model-simple-box_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Box_model__0_box-model-simple-box_0_phone.png",
+        "selector": "#box-model-simple-box",
+        "fileName": "css_kata_Box_model__0_box-model-simple-box_0_phone.png",
+        "label": "Box model: ",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 15,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/css_kata_Box_model__0_box-model-simple-border-box_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Box_model__0_box-model-simple-border-box_0_phone.png",
+        "selector": "#box-model-simple-border-box",
+        "fileName": "css_kata_Box_model__0_box-model-simple-border-box_0_phone.png",
+        "label": "Box model: ",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 17,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/css_kata_Box_model__0_box-model-simple-border-margin-box_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Box_model__0_box-model-simple-border-margin-box_0_phone.png",
+        "selector": "#box-model-simple-border-margin-box",
+        "fileName": "css_kata_Box_model__0_box-model-simple-border-margin-box_0_phone.png",
+        "label": "Box model: ",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 15,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
         "reference": "../bitmaps_reference/css_kata_Shapes_Boxes_0_box-align_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Shapes_Boxes_0_box-align_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Shapes_Boxes_0_box-align_0_phone.png",
         "selector": "#box-align",
         "fileName": "css_kata_Shapes_Boxes_0_box-align_0_phone.png",
         "label": "Shapes: Boxes",
@@ -172,7 +235,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/css_kata_Shapes_Circles_0_circle-align_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Shapes_Circles_0_circle-align_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Shapes_Circles_0_circle-align_0_phone.png",
         "selector": "#circle-align",
         "fileName": "css_kata_Shapes_Circles_0_circle-align_0_phone.png",
         "label": "Shapes: Circles",
@@ -184,7 +247,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 8,
+          "analysisTime": 10,
           "getDiffImage": null
         }
       },
@@ -193,7 +256,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/css_kata_Shapes_Circle_Outlines_0_circle-outlines-align_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Shapes_Circle_Outlines_0_circle-outlines-align_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Shapes_Circle_Outlines_0_circle-outlines-align_0_phone.png",
         "selector": "#circle-outlines-align",
         "fileName": "css_kata_Shapes_Circle_Outlines_0_circle-outlines-align_0_phone.png",
         "label": "Shapes: Circle Outlines",
@@ -214,7 +277,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/css_kata_Shapes_Combo_0_shapes-align_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Shapes_Combo_0_shapes-align_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Shapes_Combo_0_shapes-align_0_phone.png",
         "selector": "#shapes-align",
         "fileName": "css_kata_Shapes_Combo_0_shapes-align_0_phone.png",
         "label": "Shapes: Combo",
@@ -226,7 +289,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 9,
+          "analysisTime": 10,
           "getDiffImage": null
         }
       },
@@ -235,30 +298,9 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/css_kata_Position_Setup_0_position-setup_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Position_Setup_0_position-setup_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Position_Setup_0_position-setup_0_phone.png",
         "selector": "#position-setup",
         "fileName": "css_kata_Position_Setup_0_position-setup_0_phone.png",
-        "label": "Position: Setup",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 18,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/css_kata_Position_Setup_0_position-bottom_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Position_Setup_0_position-bottom_0_phone.png",
-        "selector": "#position-bottom",
-        "fileName": "css_kata_Position_Setup_0_position-bottom_0_phone.png",
         "label": "Position: Setup",
         "misMatchThreshold": 0.1,
         "diff": {
@@ -276,8 +318,29 @@ report({
     },
     {
       "pair": {
+        "reference": "../bitmaps_reference/css_kata_Position_Setup_0_position-bottom_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Position_Setup_0_position-bottom_0_phone.png",
+        "selector": "#position-bottom",
+        "fileName": "css_kata_Position_Setup_0_position-bottom_0_phone.png",
+        "label": "Position: Setup",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 19,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
         "reference": "../bitmaps_reference/css_kata_Position_Setup_0_position-override_0_phone.png",
-        "test": "../bitmaps_test/20170425-094516/css_kata_Position_Setup_0_position-override_0_phone.png",
+        "test": "../bitmaps_test/20170424-205536/css_kata_Position_Setup_0_position-override_0_phone.png",
         "selector": "#position-override",
         "fileName": "css_kata_Position_Setup_0_position-override_0_phone.png",
         "label": "Position: Setup",
@@ -289,7 +352,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 18,
+          "analysisTime": 21,
           "getDiffImage": null
         }
       },
